b4c256a5548a8baa000bd2e4265ea40c2841f771
SamuelDaigle <samuel_daigle@outlook.com>
2016-04-10 16:17:02 -0700

    a

--- js/dot.js
+++ js/dot.js
@@ -4,13 +4,9 @@ class Dot {
 	this.y = y;
   }
   
-  function update() {
-	  x += 1;
-	  y -= 1;
-  }
-  
-  function draw() {
-    update();
+  draw() {
+    x += 1;
+	y -= 1;
 	
 	ctx.fillStyle = "rgba(30, 144, 255, " + 0.25 / y + ")";
 	ctx.beginPath();
