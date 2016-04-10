fa819dd6f7c87eca1a76226e6ff68afe784ba2d5
Samuel Daigle <samuel_daigle@outlook.com>
2016-04-10 09:49:51 -0400

    function

--- js/dots.js
+++ js/dots.js
@@ -1,10 +1,17 @@
 function DrawDots(nbX, spacing, randomMin, randomMax)
 {
-	var canvas = document.getElementById('dotscanvas');
-	var ctx = canvas.getContext('2d');
+	var canvas = document.getElementById("dotscanvas");
+	canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+			
+    if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+		
+		ctx.fillStyle = "rgba(30, 144, 255, 0.25)";
+		ctx.beginPath();
+		ctx.arc(10, 10, 10, 0, Math.PI * 2, true);
+		ctx.fill();
+    }
 	
-	ctx.fillStyle = "white";
-	ctx.fillRect (10, 20, 50, 50);
-	
-	//var loopTimer = setTimeout('draw('+nbX+','+spacing+','+randomMin+','+randomMax+')', 20);
+	var loopTimer = setTimeout('draw('+nbX+','+spacing+','+randomMin+','+randomMax+')', 20);
 }
