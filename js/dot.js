798b2d0dff3fff47ee93b388212a697cdfc6fc1c
SamuelDaigle <samuel_daigle@outlook.com>
2016-04-10 16:07:28 -0700

    Animate

---
+++ js/dot.js
@@ -0,0 +1,20 @@
+class Dot { 
+  constructor(x, y) {
+    this.x = x;
+	this.y = y;
+  }
+  
+  draw() {
+    update();
+	
+	ctx.fillStyle = "rgba(30, 144, 255, " + 0.25 / y + ")";
+	ctx.beginPath();
+	ctx.arc(spacing * x, canvas.height - spacing * y - 5, 10, 0, Math.PI * 2, true);
+	ctx.fill();
+  }
+  
+  update() {
+	  x += 1;
+	  y -= 1;
+  }
+}
