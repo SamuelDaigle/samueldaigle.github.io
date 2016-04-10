58866bd36459acce92038fdf12366149377824d9
SamuelDaigle <samuel_daigle@outlook.com>
2016-04-10 16:55:27 -0700

    removed alpha

--- js/dot.js
+++ js/dot.js
@@ -8,7 +8,7 @@ class Dot {
     this.x += 1;
 	this.y -= 1;
 	
-	ctx.fillStyle = "rgba(30, 144, 255, " + 0.25 / this.y + ")";
+	ctx.fillStyle = "rgba(30, 144, 255, " + 0.25 + ")";
 	ctx.beginPath();
 	ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, true);
 	ctx.fill();
