158cded0cfcaa68641e2642f7b82bee22e031650
SamuelDaigle <samuel_daigle@outlook.com>
2016-04-10 17:38:09 -0700

    a

--- js/dot.js
+++ js/dot.js
@@ -10,7 +10,7 @@ class Dot {
     this.x += this.lookatX;
 	this.y -= this.lookatY;
 	
-	ctx.fillStyle = "rgba(30, 144, 255, " + -this.y + 200 + ")";
+	ctx.fillStyle = "rgba(30, 144, 255, " + -this.y + window.innerHeight / 2 + ")";
 	ctx.beginPath();
 	ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, true);
 	ctx.fill();
