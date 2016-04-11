5ef3a905b0d00ffd0a649cd3fada633bdfa8687e
SamuelDaigle <samuel_daigle@outlook.com>
2016-04-10 17:16:18 -0700

    a

--- js/dot.js
+++ js/dot.js
@@ -3,12 +3,12 @@ class Dot {
     this.x = x;
 	this.y = y;
 	this.lookatX = Math.floor(Math.random() * 2) - 1;
-	this.lookatY = Math.floor(Math.random() * 2) - 0.5;
+	this.lookatY = Math.abs(Math.floor(Math.random() * 2) - 0.5);
   }
   
   draw(ctx) {
     this.x += this.lookatX;
-	this.y += this.lookatY;
+	this.y -= this.lookatY;
 	
 	ctx.fillStyle = "rgba(30, 144, 255, " + 0.25 + ")";
 	ctx.beginPath();
