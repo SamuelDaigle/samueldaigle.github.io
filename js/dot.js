e6935e7470c1038ed91e68c848bbbeba90c20c5a
SamuelDaigle <samuel_daigle@outlook.com>
2016-04-11 19:26:17 -0400

    random direction

--- js/dot.js
+++ js/dot.js
@@ -9,17 +9,10 @@ class Dot {
   }
   
   draw(ctx) {
+	this.direction = Math.floor((Math.random() * 2) - 1);
+	
     this.x += this.direction;
 	this.y -= 1;
-	
-	if (this.x > this.maxX)
-	{
-		this.direction = -1;
-	}
-	else if (this.x < this.minX)
-	{
-		this.direction = 1;
-	}
 	
 	this.alpha -= 0.10;
 	
